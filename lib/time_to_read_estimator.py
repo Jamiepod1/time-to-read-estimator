b5b550f6c300f819cc7e7b52b481c01f0b0c3610
Jamie <jamieplumy@gmail.com>
2024-10-02 16:54:48 +0100

    Refactor, maybe better

--- lib/time_to_read_estimator.py
+++ lib/time_to_read_estimator.py
@@ -5,50 +5,44 @@ def time_to_read_estimate(text, words_per_minute=200):
     if type(text) != str:
         raise Exception("Invalid input type, must be str")
     
-    
     words = text.split()
     number_of_words = len(words)
     if number_of_words == 0:
         return "Nothing to read!"
     
-    
     words_per_second = words_per_minute / 60
-    seconds_to_read = number_of_words / words_per_second
-
-    rounded_seconds_to_read = math.ceil(seconds_to_read)
-        
-    minutes_to_read = rounded_seconds_to_read / 60
-    hours_to_read = minutes_to_read / 60
-
-    if hours_to_read >= 1:
-        hours_to_read = int(hours_to_read)
-        output_string = f"{hours_to_read} Hour"
-        remaining_minutes_to_read = int(minutes_to_read) % 60
-        if remaining_minutes_to_read > 0:
-            output_string += f", {remaining_minutes_to_read} Minute"
-            
-
-        remaining_seconds_to_read = rounded_seconds_to_read % 60
-        if remaining_seconds_to_read > 0:
-            output_string += f", {remaining_seconds_to_read} Second"
-        if remaining_seconds_to_read > 1:
-            output_string += "s"
-        
-
-    elif minutes_to_read >= 1:
-        minutes_to_read = int(minutes_to_read)
-        remaining_seconds_to_read = rounded_seconds_to_read % 60
-        output_string = f"{minutes_to_read} Minute"
-        if minutes_to_read > 1:
-            output_string += "s"
-        if remaining_seconds_to_read > 0:
-            output_string += f", {remaining_seconds_to_read} Second"
-        if remaining_seconds_to_read > 1:
-            output_string += "s"
     
-    elif rounded_seconds_to_read > 1:
-        output_string = f"{rounded_seconds_to_read} Seconds"
-    else:
-        output_string = f"{rounded_seconds_to_read} Second"
+    seconds = number_of_words / words_per_second
+    rounded_seconds = math.ceil(seconds)
+    minutes = rounded_seconds / 60
+    hours = minutes / 60
+    
+    output_list = []
+    if hours >= 1:
+        hours = int(hours)
+        minutes = int(minutes) % 60
+        rounded_seconds = rounded_seconds % 3600
+        hour_string = f"{hours} Hour"
+        if hours > 1:
+            hour_string += "s"
+            
+        output_list.append(hour_string)
+
+    if minutes >= 1:
+        minutes = int(minutes)
+        minute_string = f"{minutes} Minute"
+        rounded_seconds %= 60
+        if minutes > 1:
+            minute_string += "s"
+        output_list.append(minute_string)
         
-    return output_string
+    if rounded_seconds > 0:
+        second_string = f"{rounded_seconds} Second"
+        if rounded_seconds > 1:
+            second_string += "s"
+        output_list.append(second_string)
+        
+    return ", ".join(output_list)
+
+
+# print(time_to_read_estimate(" ".join(["word" for num in range(0, 12000)])))
